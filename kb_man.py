446e522be2ba1ba66cf02acc3ec3f56ae42b1684
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-11-01 15:09:20 +0530

    `kb_man.py`: Checker interprets backspace
    A Checker object can now understand backspace
    and behave correctly.

--- kb_man.py
+++ kb_man.py
@@ -7,21 +7,32 @@ class Checker:
     the user.'''
 
     content_file = ""
-    queue = Queue()
+    content = ""
+    index = 0
 
-    def __init__(content_file="content.txt"):
+    def __init__(self, content_file="content.txt"):
         self.content_file = content_file
         with open(content_file, 'r') as f:
-            txt = f.read()
-            for s in txt:
-                queue.put(s, False)
+            self.content = f.read()
 
-    def check(c: str):
+    def __next__(self) -> str:
+        s = self.content[self.index]
+        self.index += 1
+        return s
+
+    def __prev__(self) -> None:
+        self.index -= 1 if self.index > 0 else 0
+
+    def check(self, c: str) -> str:
         '''Check the if input character is correct, else rubricate and mock.'''
-        if c == queue.get(False):
+        if c == '\b':
+            self.__prev__()
+            return "\b \b"
+        elif c == self.__next__():
             return c
         else:
             return Fore.RED + c + Fore.RESET
+
 
 key_repr = {
     "space": " ",
@@ -31,12 +42,12 @@ key_repr = {
 }
 
 # Press and release event actions.
+content_file = "content.txt"
+checker = Checker(content_file)
 def on_press(key):
     k = key_repr.get(key, key)
-    if k == "\b":
-        print(f"\b \b", end='', flush=True)
-    else:
-        print(k, end='', flush=True)
+    k = checker.check(k)
+    print(k, end='', flush=True)
 
 def on_release(key):
     pass
@@ -44,12 +55,14 @@ def on_release(key):
 # Here follows the start of execution.
 def main():
     try:
+        with open(content_file) as f:
+            print(f.read() + "\n", flush=True)
         listen_keyboard(
                 on_press=on_press,
                 on_release=on_release,
                 delay_second_char=0.1)
     except KeyboardInterrupt:
-        print("\nQuitting ...")
+        print("Quitting ...")
         exit(0);
 
 if __name__ == "__main__":
