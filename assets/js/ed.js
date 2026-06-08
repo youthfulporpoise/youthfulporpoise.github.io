d3070eb9297d0f5e6677c502f1bae4082141ac74
youthfulporpoise <hatalihim7d4@protonmail.com>
2026-06-08 19:42:10 +0530

    heaven spacing removed

--- assets/js/ed.js
+++ assets/js/ed.js
@@ -63,12 +63,9 @@ function setupImageLightBox() {
 }
 
 function fixBottomVoid() {
-  console.log("Fixing Bottom Void");
   const bottomElement = document.getElementById("bottom-element");
   const voidMark = document.getElementById("void-mark");
-  const heavenMark = document.getElementById("heaven-mark");
 
-  console.log("Found Bottom Element" + bottomElement + ", " + voidMark);
   const rect = bottomElement.getBoundingClientRect();
   const vpHeight = window.innerHeight;
 
@@ -77,17 +74,10 @@ function fixBottomVoid() {
 
     const voidSpacer = document.createElement("div");
     voidSpacer.id = "void-spacer";
-    voidSpacer.style.height = Math.floor(gap / 2) + "px";
+    voidSpacer.style.height = gap + "px";
     voidSpacer.style.pointerEvents = "none";
 
-    const heavenSpacer = document.createElement("div");
-    heavenSpacer.id = "heaven-spacer";
-    heavenSpacer.style.height = Math.ceil(gap / 2) + "px"
-    heavenSpacer.style.pointerEvents = "none";
-
-    console.log("Inserting spaces of " + gap / 2 + "px");
     voidMark.parentNode.insertBefore(voidSpacer, voidMark);
-    heavenMark.parentNode.insertBefore(heavenSpacer, heavenMark);
   }
 }
 
