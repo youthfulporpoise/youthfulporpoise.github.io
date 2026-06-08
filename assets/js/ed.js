dd59c16f015ff3695bf1b1d13d6f421060de5ea2
youthfulporpoise <hatalihim7d4@protonmail.com>
2026-06-08 18:56:48 +0530

    dynamically remove bottom void

--- assets/js/ed.js
+++ assets/js/ed.js
@@ -62,9 +62,62 @@ function setupImageLightBox() {
   });
 }
 
+function fixBottomVoid() {
+  console.log("Fixing Bottom Void");
+  const bottomElement = document.getElementById("bottom-element");
+  const voidMark = document.getElementById("void-mark");
+  const heavenMark = document.getElementById("heaven-mark");
+
+  console.log("Found Bottom Element" + bottomElement + ", " + voidMark);
+  const rect = bottomElement.getBoundingClientRect();
+  const vpHeight = window.innerHeight;
+
+  if (rect.bottom < vpHeight) {
+    const gap = vpHeight - rect.bottom;
+
+    const voidSpacer = document.createElement("div");
+    voidSpacer.id = "void-spacer";
+    voidSpacer.style.height = Math.floor(gap / 2) + "px";
+    voidSpacer.style.pointerEvents = "none";
+
+    const heavenSpacer = document.createElement("div");
+    heavenSpacer.id = "heaven-spacer";
+    heavenSpacer.style.height = Math.ceil(gap / 2) + "px"
+    heavenSpacer.style.pointerEvents = "none";
+
+    console.log("Inserting spaces of " + gap / 2 + "px");
+    voidMark.parentNode.insertBefore(voidSpacer, voidMark);
+    heavenMark.parentNode.insertBefore(heavenSpacer, heavenMark);
+  } else {
+    const voidSpacer = document.getElementById("void-spacer");
+    const heavenSpacer = document.getElementById("heavens-spacer");
+    if (voidSpacer)
+      voidSpacer.remove();
+    if (heavenSpacer)
+      heavenSpacer.remove();
+  }
+}
+
+function debounce(func, delay) {
+  let timerId;
+  return function(...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, delay);
+  }
+}
+
+const fixBottomVoidDebounced = debounce(fixBottomVoid, 1000);
+
 document.addEventListener('DOMContentLoaded', () => {
   setupBackToTop();
   setupHypothes();
   setupSideBarAutoClose();
   setupImageLightBox();
+  fixBottomVoid();
+  
+  window.addEventListener("resize", () => {
+    fixBottomVoidDebounced();
+  });
 });
